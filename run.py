8336890720391f0706f867f6fab19fb92ce23cd4
Kiki Wijaya <63471866+kiki-boki@users.noreply.github.com>
2022-10-27 21:40:26 +0700

    Update run.py

--- run.py
+++ run.py
@@ -56,17 +56,15 @@ def wijaya(s):
         time.sleep(random.random() * 0.2)
 #----------------------------------#
 def logo():
-    clear()
-    print(f"""{x}
+    boki(f"""{x}
 {u}   ______                __   _                                  
 {u}  / ____/________ ______/ /__(_)___  ____ _  
  {p}/ /   / ___/ __ `/ ___/ //_/ / __ \/ __ `/ {x}=== Kiki Wijaya
 {u}/ /___/ /  / /_/ / /__/ ,< / / / / / /_/ /  {x}=== {M}INDONESIA
 {p}\____/_/   \__,_/\___/_/|_/_/_/_/_/\__, /   {x}=== {p}2022
           {u}/  _/___  _____/ /__ ____ ___/ __________ _____ ___ 
-          {p}/ // __ \/ ___/ __/ __ `/ __ `/ ___/ __ `/ __ `__ \
-        {u}
-        _/ // / / (__  ) /_/ /_/ / /_/ / /  / /_/ / / / / / /
+          {p}/ // __ \/ ___/ __/ __ `/ __ `/ ___/ __ `/ __ `__ \ 
+    {u}    _/ // / / (__  ) /_/ /_/ / /_/ / /  / /_/ / / / / / /
        {p}/___/_/ /_/____/\__/\__,_/\__, /_/   \__,_/_/ /_/ /_/ 
                                 /____/ 
 {x}           +======================================+
@@ -74,7 +72,7 @@ def logo():
            |{H}-{x} Whatsapp  :  083192495253           |
            |{H}-{x} Contack   :  kikimskj935@gmail.com  |
            |{H}-{x} MyInsta   :  bokiofficial88         |
-           +======================================+""")
+           +======================================+\n""")
 #--------------------------------------------------#
 def notprem():
     clear()
@@ -85,15 +83,12 @@ def notprem():
 def kiki():
     clear()
     logo()
-    cetak(nel('[[green]01[white]] Chat Admin Untuk Membeli Tools\n[[green]02[white]] Informasi Tools'))
+    cetak(nel('[[green]01[white]] Lihat List Harga Tools\n[[green]02[white]] Informasi Tools'))
     
     anu = input(f'{u}[{h}+{u}]{x} Pilih : ')
     
     if anu in ['1','01']:
-        boki(f'Anda Akan Di Arah Kan Ke {h}WhatsApp{x}')
-        time.sleep(2)
-        os.system('xdg-open https://wa.me/6283192495253?text=Assalamualaikum+Bang+Kiki+Saya+Ingin+Membeli+Tools+Crack+Instagram')
-        exit()
+        pilat()
     elif anu in ['2','02']:
         cetak(nel('\tNOTE'))
         wijaya(f'{u}[{h}+{u}]{x}Tools Ini Dibuat Untuk Crack Instagram Atau Hack Account Secara Random\n\t{m}This is a Legal Program!!!{x}\n{u}[{h}+{u}]{x}Jadi Anda Mau Berbuat Jahat Atau Kebaikan Itu Resiko Anda')
@@ -101,6 +96,39 @@ def kiki():
         cetak(nel('[green]Press Enter To Continue.......'))
         input(' ')
         time.sleep(2)
+        kiki()
+#-----------------------------#
+def pilat():
+    clear()
+    boki("""
+My Instagram  :  
+\t _           _    _        __  __ _      _       _  ___   ___  
+\t| |__   ___ | | _(_) ___  / _|/ _(_) ___(_) __ _| |( _ ) ( _ ) 
+\t| '_ \ / _ \| |/ / |/ _ \| |_| |_| |/ __| |/ _` | |/ _ \ / _ \ 
+\t| |_) | (_) |   <| | (_) |  _|  _| | (__| | (_| | | (_) | (_) |
+\t|_.__/ \___/|_|\_\_|\___/|_| |_| |_|\___|_|\__,_|_|\___/ \___/ 
+                                                               \n""")
+    au=' (\x1b[1;92m01\33[m) \033[33mRp.50K\33[m  Tools Permanen\n (\x1b[1;92m02\33[m) \033[33mRp.25K\33[m  Tools Perbulan\n (\x1b[1;92m03\33[m) \033[33mRp.100K\33[m Tools Open Source'
+    men=nel(au,style="")
+    cetak(nel(men, title='List Harga Tools'))
+    wow = input(f'Pilih Harga Tools : ')
+    if wow in ['1','01']:
+        boki(f'Anda Akan Di Arah Kan Ke {h}WhatsApp{x}')
+        time.sleep(2)
+        os.system('xdg-open https://wa.me/6283192495253?text=Assalamualaikum+Bang+Kiki+Saya+Ingin+Membeli+Tools+Crack+Instagram+Permanen+Bang')
+        exit()
+    elif wow in ['2','02']:
+        boki(f'Anda Akan Di Arah Kan Ke {h}WhatsApp{x}')
+        time.sleep(2)
+        os.system('xdg-open https://wa.me/6283192495253?text=Assalamualaikum+Bang+Kiki+Saya+Ingin+Membeli+Tools+Crack+Instagram+Perminggu+Bang')
+        exit()
+    elif wow in ['3','03']:
+        boki(f'Anda Akan Di Arah Kan Ke {h}WhatsApp{x}')
+        time.sleep(2)
+        os.system('xdg-open https://wa.me/6283192495253?text=Assalamualaikum+Bang+Kiki+Saya+Ingin+Membeli+Tools+Crack+Instagram+Open+Source+Bang')
+        exit()
+    elif wow in ['']:
+        boki(f'Jangan Kosong Bang{m}!!!')
         kiki()
 #----------------------------------------------------------------------#
 if __name__=='__main__':
